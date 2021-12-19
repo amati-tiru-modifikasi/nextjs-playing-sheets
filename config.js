6e4f5b4ecbb60a6d5fdd48b27b787ee65df06d6b
mdestafadilah <desta.08b@gmail.com>
2021-12-19 22:18:45 +0700

    update

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 export default {
-  title: "Next.js Chakra UI Template",
-  description: "A starter Template for Next.js with Chakra UI.",
+  title: "Next.js Playing Google Sheets",
+  description: "A NextJS Played, with Google Sheets API as Database Service.",
 };
 
 export const links = [
   { title: "Home", path: "/" },
-  { title: "About", path: "/About" },
-  { title: "Contact", path: "/contact" },
 ];
